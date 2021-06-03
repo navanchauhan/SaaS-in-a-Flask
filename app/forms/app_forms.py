53ae62ef3f206907f89aa3088b83b638b59e6de5
Navan Chauhan <navanchauhan@gmail.com>
2021-06-03 23:45:51 +0530

    added user sign up form

--- app/forms/app_forms.py
+++ app/forms/app_forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, PasswordField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email
 
@@ -8,3 +8,9 @@ class MyForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired(), Email()])
     subject = StringField('Subject', validators=[DataRequired()])
     body = TextAreaField('Message', validators=[DataRequired()])
+
+class UserSignUp(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name')
+    email = EmailField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
