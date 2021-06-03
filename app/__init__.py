5525ecf681b83f2393dfda458d39f562ee3eba09
Navan Chauhan <navanchauhan@gmail.com>
2021-06-03 22:49:20 +0530

    moved user db model to seperate file

--- app/__init__.py
+++ app/__init__.py
@@ -10,13 +10,7 @@ app.config.from_object('app.config')
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), unique=True, nullable=False)
-	email = db.Column(db.String(120), unique=True, nullable=False)
-
-	def __repr__(self):
-		return '<User %r>' % self.username
+from app.models import User
 
 database_cli = AppGroup("database")
 @database_cli.command("create")
---
+++ app/models.py
@@ -0,0 +1,7 @@
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(80), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+
+	def __repr__(self):
+		return '<User %r>' % self.username
