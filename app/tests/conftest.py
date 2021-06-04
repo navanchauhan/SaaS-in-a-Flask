7df72bb7d053f05599a04922107512bd239f382f
Navan Chauhan <navanchauhan@gmail.com>
2021-06-04 15:13:59 +0530

    disable csrf while testing and create dummy db

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 
 from app import app as flask_app
+from app import db
+import tempfile
+import os
 
 @pytest.fixture
 def app():
+	flask_app.config['WTF_CSRF_ENABLED'] = False
+	flask_app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + tempfile.mkstemp(suffix='.db')[-1]
+	db.create_all()
 	yield flask_app
 
 @pytest.fixture
