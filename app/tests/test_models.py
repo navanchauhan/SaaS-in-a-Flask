7ffc4f27431e0434819a47f85d61b877d346700d
Navan Chauhan <navanchauhan@gmail.com>
2021-06-04 15:45:19 +0530

    add test for models

---
+++ app/tests/test_models.py
@@ -0,0 +1,11 @@
+from app.models import User
+
+def test_usermodel(app,client):
+	user = User(
+		first_name="John",
+		email="test@example.com",
+		password="pass")
+	assert user.full_name == "John None"
+	assert user.get_role() == None
+	assert user.get_team() == None
+	assert user.is_paid() == None
