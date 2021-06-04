9f4635b6e9f5a4bcc48dd754c52cb5d91c1810a5
Navan Chauhan <navanchauhan@gmail.com>
2021-06-04 16:04:30 +0530

    added test for cli commands

---
+++ app/tests/test_cli.py
@@ -0,0 +1,6 @@
+from app import database_cli
+
+def test_database_commands(app, client):
+	runner = app.test_cli_runner()
+	assert runner.invoke(database_cli,["delete"]).exit_code == 0
+	assert runner.invoke(database_cli,["create"]).exit_code == 0
