5d2cbf0f3420a1e443b4d15e9dd938b0c1c11d68
Navan Chauhan <navanchauhan@gmail.com>
2021-06-06 14:57:09 +0530

    send mail with threading

--- app/misc_func.py
+++ app/misc_func.py
@@ -1,4 +1,7 @@
+from threading import Thread
 from flask import flash
+from flask_mailman import EmailMultiAlternatives
+from app import app, mail
 
 def flash_errors(form):
     for field, errors in form.errors.items():
@@ -7,3 +10,18 @@ def flash_errors(form):
                 getattr(form, field).label.text,
                 error
             ), 'danger')
+
+# Sauce: https://github.com/alectrocute/flaskSaaS/blob/master/app/toolbox/email.py 
+
+def send(to, subject, body, body_html,thread=True):
+    sender = app.config["MAIL_FROM"] 
+    message = EmailMultiAlternatives(
+        subject,body,sender,[to])
+    message.attach_alternative(body_html,"text/html")
+    thr = Thread(target=send_async, args=[app,message])
+    thr.start()
+
+def send_async(app, message):
+    with app.app_context():
+        message.send()
+        print("Mail Sent")
