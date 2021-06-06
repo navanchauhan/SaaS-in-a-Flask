a1a5fcdcd2380d7130938dd69afd87eb7b544d2e
Navan Chauhan <navanchauhan@gmail.com>
2021-06-06 14:57:56 +0530

    config for mail setup

--- app/config_dev.py
+++ app/config_dev.py
@@ -10,3 +10,14 @@ ADMIN_PASSWORD = "iamgroot"
 
 GOOGLE_CLIENT_ID=os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET=os.environ.get("GOOGLE_CLIENT_SECRET")
+
+
+#MAIL_BACKEND = 'file'
+#MAIL_FILE_PATH = '/tmp/app-messages'
+MAIL_BACKEND = "smtp"
+MAIL_SERVER = os.environ.get("MAIL_SERVER")
+MAIL_PORT = os.environ.get("MAIL_PORT") 
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+MAIL_FROM = os.environ.get("MAIL_FROM")
+MAIL_USE_TLS = bool(os.environ.get("MAIL_USE_TLS"))
