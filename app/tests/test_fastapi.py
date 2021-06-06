ff23de920e1c0837bbc1a82799feeaef9c83a723
Navan Chauhan <navanchauhan@gmail.com>
2021-06-06 15:22:02 +0530

    testing if user does not exist

--- app/tests/test_fastapi.py
+++ app/tests/test_fastapi.py
@@ -8,3 +8,8 @@ def test_fastapi(app, client):
 	res = fastapi_client.get("/version")
 	assert res.status_code == 200
 	assert res.json() == {"message":fastapi_app.version}	
+
+	res = fastapi_client.get("/v1/user-details?email={}".format("notadmin@example.com"))
+	print(res.json())
+	assert res.status_code == 404 
+	assert res.json()["detail"] == "User Not Found"	
