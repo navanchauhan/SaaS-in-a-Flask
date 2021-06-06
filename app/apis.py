d3ab83d7d10a06697fa0b764d11166fcfbd16cc6
Navan Chauhan <navanchauhan@gmail.com>
2021-06-06 15:54:14 +0530

    removed assertion

--- app/apis.py
+++ app/apis.py
@@ -13,8 +13,6 @@ async def API_Version():
 @app.get("/v1/user-details")
 async def API_User_Details(email: str):
     user = models.User.query.filter_by(email=email).first()
-    try:
-        assert user is not None
-    except AssertionError:
-        raise HTTPException(status_code=404, detail="User Not Found")
+    if user is None:
+        raise HTTPException(status_code=404,detail="User Not Found")
     return {"first_name": user.first_name, "last_name": user.last_name}
